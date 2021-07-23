13dd2841738fe62607412eea635271755599cacc
BlackNinja5674 <67200367+BlackNinja5674@users.noreply.github.com>
2021-07-23 13:14:45 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -76,7 +76,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Share Bots', url='http://t.me/share/url?url=Checkout%20%40Seriessearch_bot%20for%20searching%20files')
+            InlineKeyboardButton('Share Bot', url='http://t.me/share/url?url=Checkout%20%40Seriessearch_bot%20for%20searching%20files')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
