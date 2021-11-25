3665fa5d4570a1a7e51bcfeeb24fe2672ddeece9
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2021-11-25 06:29:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,6 +60,11 @@ async def filter(client, message):
                     )
         else:
             await client.send_message(chat_id=message.from_user.id, text="Sorry, The file name you requested doesnt seem available, Try checking the spelling or Request the series or movies in [our group](https://t.me/requestyourseries_movies).")
+            btn.append(
+                    [
+                        InlineKeyboardButton('Request Group', url='https://t.me/requestyourseries_movies')
+                    ]
+                    )
             return
 
         if not btn:
